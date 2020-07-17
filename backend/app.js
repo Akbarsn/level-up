dd0aab0a677182be914a4f8cf6c4a0a0370cd46a
Akbarsn <personal.akbarsn@gmail.com>
2020-07-17 16:07:16 +0700

    Integrate ESLint & add command

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
-const express = require("express")
-const app = express()
-const PORT = 5000
+require('dotenv').config()
 
-app.get("/ping", (req, res, next) => {
-    res.json({
-        message: "Pong"
-    })
+const express = require('express')
+const app = express()
+const PORT = process.env.NODE_ENV !== 'DEVELOPMENT' ? process.env.PORT : 5000
+
+app.get('/ping', (req, res, next) => {
+  res.json({
+    message: 'Pong'
+  })
 })
 
 app.listen(PORT, () => {
-    console.log(`Listening to ${PORT}`)
+  console.log(`Listening to ${PORT}`)
 })
