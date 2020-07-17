3675b1499289d2e4d0ef820e83ee64d5eca6b959
Akbarsn <personal.akbarsn@gmail.com>
2020-07-17 16:07:26 +0700

    Add initial test

---
+++ backend/test/ServerTest.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+const chai = require('chai')
+const expect = chai.expect
+const chaiHttp = require('chai-http')
+
+chai.use(chaiHttp)
+
+describe('Test Initiated', () => {
+  it('Should Return Pong Message', (done) => {
+    chai.request('localhost:5000')
+      .get('/ping')
+      .end((_err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body).to.have.property('message').eql('Pong')
+        done()
+      })
+  })
+})
