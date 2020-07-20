e4adaf8611de857b81e4ef660e0e1689828ca84f
Akbarsn <personal.akbarsn@gmail.com>
2020-07-20 08:07:14 +0700

    Create test for homepage

--- backend/test/HomepageTest.js
+++ backend/test/HomepageTest.js
@@ -7,13 +7,14 @@ const chaiHttp = require('chai-http')
 const model = require('../models')
 const jwt = require('jsonwebtoken')
 const JWT_KEY = process.env.JWT_KEY
+const PORT = process.env.PORT
 
 const expect = chai.expect
 chai.use(chaiHttp)
 
 describe('Get Homepage Data', () => {
-    it('Homepage OK', (done) => {
-        const user = model.findOne({
+    it('Homepage OK', async (done) => {
+        const user = await model.User.findOne({
             where: {
                 username: 'akbarsn'
             }
@@ -28,7 +29,7 @@ describe('Get Homepage Data', () => {
 
         chai.request(`localhost:${PORT}`)
             .get('/homepage')
-            .set('token', `bearer ${token}`)
+            .set('authorization', `bearer ${token}`)
             .end((err, res) => {
                 expect(res).to.have.status(200)
                 expect(res.body).to.have.property('full_name')
@@ -38,7 +39,7 @@ describe('Get Homepage Data', () => {
 
                 expect(res.body.tasks).to.be.an('array')
                 expect(res.body.habits).to.be.an('array')
-                expect(res.body.saving).to.be.an('object')
+                // expect(res.body.saving).to.be.an('object')
 
                 if (err) {
                     done(err)
