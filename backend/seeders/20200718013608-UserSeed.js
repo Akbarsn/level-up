0b349bfe796a72ff55539f870606d68884c36023
Akbarsn <personal.akbarsn@gmail.com>
2020-07-26 09:52:07 +0700

    Add one user in seeder

--- backend/seeders/20200718013608-UserSeed.js
+++ backend/seeders/20200718013608-UserSeed.js
@@ -4,14 +4,24 @@ const bcrypt = require('bcrypt')
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.bulkInsert('Users', [{
-            full_name: 'Akbar S N',
-            email: 'personal.akbarsn@gmail.com',
-            username: 'akbarsn',
-            password: bcrypt.hashSync('123456', 12),
-            createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
-            updatedAt: moment().format('YYYY-MM-DD hh:mm:ss')
-        }], {})
+        await queryInterface.bulkInsert('Users', [
+            {
+                full_name: 'Akbar S N',
+                email: 'personal.akbarsn@gmail.com',
+                username: 'akbarsn',
+                password: bcrypt.hashSync('123456', 12),
+                createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
+                updatedAt: moment().format('YYYY-MM-DD hh:mm:ss')
+            },
+            {
+                full_name: 'John Doe',
+                email: 'johndoe@gmail.com',
+                username: 'johndoe',
+                password: bcrypt.hashSync('123456', 12),
+                createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
+                updatedAt: moment().format('YYYY-MM-DD hh:mm:ss')
+            }
+        ], {})
     },
 
     down: async (queryInterface, Sequelize) => {
